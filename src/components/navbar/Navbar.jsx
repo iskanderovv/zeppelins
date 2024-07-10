0e2b19b4fd8d4de1f6c7ffaa73479513c808b398
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-07-10 16:46:07 +0500

    added language to sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,71 +8,32 @@ import Container from '../../utils/container/Container';
 import logo from '../../images/logo.svg';
 import ReactFlagsSelect from 'react-flags-select';
 import { useTranslation } from 'react-i18next';
-import i18n from '../../locales/i18next'
-
-
 
 export default function Navbar() {
   const { t, i18n } = useTranslation();
-  const [selected, setSelected] = useState("");
   const [state, setState] = useState({
     right: false,
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
-
+    if (
+      event &&
+      event.target.closest('.MuiDrawer-paper') &&
+      !event.target.closest('.MuiDrawer-paper .react-flags-select')
+    ) {
+      return;
+    }
+  
     setState({ ...state, [anchor]: open });
   };
+  
 
-  const list = (anchor) => (
-    <Box
-      sx={{ width: '360px' }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        px: '16px'
-      }}>
-        <Link to='/' className='flex-1 h-20 items-center flex'>
-          <img width={200} src={logo} alt="logo" />
-        </Link>
-        <IconButton onClick={toggleDrawer(anchor, false)}>
-          <CloseIcon />
-        </IconButton>
-      </Box>
-      <ReactFlagsSelect
-          countries={['UZ', 'RU', 'US', 'FR']}
-          customLabels={{ UZ: 'UZ', RU: 'RU', US: 'EN', FR: 'FR' }}
-          onSelect={handleSelect}
-          selected={currentLanguage}
-          placeholder={currentLanguage}
-        />
-      <List disablePadding>
-        {[
-          [t('portfolio'), '#'],
-          [t('blog'), '#'],
-          [t('cv'), '#'],
-          [t('store'), '#'],
-          [t('freelance'), '#'],
-          [t('about'), '#'],
-          [t('contact'), '#'],
-        ].map(([title, url], index) => (
-          <ListItem key={index} disablePadding>
-            <ListItemButton component={Link} to={url}>
-              <ListItemText primary={title} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   const handleSelect = (countryCode) => {
     switch (countryCode) {
@@ -94,7 +55,57 @@ export default function Navbar() {
   };
 
   const currentLanguage = i18n.language.substring(0, 2).toUpperCase();
-  
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: '360px' }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        px: '16px'
+      }}>
+        <Link to='/' className='flex-1 h-20 items-center flex'>
+          <img width={200} src={logo} alt="logo" />
+        </Link>
+        <IconButton onClick={toggleDrawer(anchor, false)} aria-label="close">
+          <CloseIcon />
+        </IconButton>
+
+      </Box>
+      <ReactFlagsSelect
+        countries={['UZ', 'RU', 'US', 'FR']}
+        customLabels={{ UZ: 'UZ', RU: 'RU', US: 'EN', FR: 'FR' }}
+        onSelect={handleSelect}
+        selected={currentLanguage}
+        placeholder={currentLanguage}
+        onClick={(e) => e.stopPropagation()}
+      />
+      <List disablePadding>
+        {[
+          [t('portfolio'), '#'],
+          [t('blog'), '#'],
+          [t('cv'), '#'],
+          [t('store'), '#'],
+          [t('freelance'), '#'],
+          [t('about'), '#'],
+          [t('contact'), '#'],
+        ].map(([title, url], index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton component={Link} to={url}>
+              <ListItemText primary={title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+
   return (
     <Container>
       <nav className='flex items-center mt-16'>
@@ -102,6 +113,9 @@ export default function Navbar() {
           anchor='right'
           open={state.right}
           onClose={toggleDrawer('right', false)}
+          ModalProps={{
+            disableScrollLock: true,
+          }}
         >
           {list('right')}
         </Drawer>
@@ -109,7 +123,7 @@ export default function Navbar() {
           <img width={260} src={logo} alt="logo" />
         </Link>
         <ReactFlagsSelect
-        className='lg:block md:block sm:block hidden'
+          className='lg:block md:block sm:block hidden'
           countries={['UZ', 'RU', 'US', 'FR']}
           customLabels={{ UZ: 'UZ', RU: 'RU', US: 'EN', FR: 'FR' }}
           onSelect={handleSelect}
